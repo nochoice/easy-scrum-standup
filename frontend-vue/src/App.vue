08d6608365c816f9f160fd3c3b93f050164f4f17
Roman Kora <roman.kora@tieto.com>
2020-09-14 09:15:40 +0200

    About the project

--- frontend-vue/src/App.vue
+++ frontend-vue/src/App.vue
@@ -1,16 +1,18 @@
 <template lang="pug">
     v-app
-        h1 Easy Scrum Standup
-        .navigation
-            center()
-                v-badge(bordered color="secondary" :content="qoutesNum" overlap)
-                    v-btn(@click="changeQoute" rounded class="white--text" color="primary" depressed) Pick random
-                div.divider or
-                v-btn(v-show="!isCategoriesSelectOpen" rounded small @click="isCategoriesSelectOpen = true") Select from categories
-            
-            quote-categories-select-component(v-show="isCategoriesSelectOpen" :categories="categories" @filteredQoute="onCategoriesSelectQoute" @close="onCategoriesSelectClose")
-        quote-component(:data="qoute")
-        quote-categories-component(:categories="categories" :categoriesAggregate="categoriesAggregate")
+        v-container
+            h1 Easy Scrum Standup
+            .navigation
+                center()
+                    v-badge(bordered color="secondary" :content="qoutesNum" overlap )
+                        v-btn(@click="changeQoute" rounded class="white--text" color="primary" depressed :disabled="isCategoriesSelectOpen") Pick random
+                    div.divider or
+                    v-btn(v-show="!isCategoriesSelectOpen" rounded small @click="isCategoriesSelectOpen = true") Select from categories
+                
+                quote-categories-select-component(v-show="isCategoriesSelectOpen" :categories="categories" @filteredQoute="onCategoriesSelectQoute" @close="onCategoriesSelectClose")
+            quote-component(:data="qoute")
+            about-component
+            //- quote-categories-component(:categories="categories" :categoriesAggregate="categoriesAggregate")
 </template>
 
 <script>
@@ -18,6 +20,7 @@
     import QuoteComponent from './components/QuoteComponent.vue';
     import QuoteCategoriesComponent from './components/QuoteCategoriesComponent.vue';
     import QuoteCategoriesSelectComponent from './components/QuoteCategoriesSelectComponent.vue';
+    import AboutComponent from './components/AboutComponent.vue';
     import { repository as qouteRepository } from './repositories/local/qoutes';
     import { repository as qouteCategoryRepository } from './repositories/local/qoute-category';
 
@@ -27,7 +30,8 @@
         components: {
             QuoteComponent,
             QuoteCategoriesComponent,
-            QuoteCategoriesSelectComponent
+            QuoteCategoriesSelectComponent,
+            AboutComponent
         },  
         data() {
             return {
@@ -66,13 +70,16 @@
 <style lang="scss" scoped>
     h1 {
         text-align: center;
-        padding: 0;
-        margin: 0;
-        font-size: 40px;
         padding: 60px 0 12px;
     }
 
     .divider {
         padding: 4px 0;
+    }
+
+    @media (min-width: 1264px) {
+        .container {
+            max-width: 772px;
+        }
     }
 </style>
---
+++ frontend-vue/src/components/AboutComponent.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+    .about()
+        h3(class="text-center mb-4") About the project
+        
+        p Did you ever find yourself in a situation that you are joining a daily (daily standup) and you realize you kind of forgot to work on the project? 
+            b Fear no more -> we got you covered.
+
+        p This project is a simple page with 
+            b random quotes 
+            | which you can use on your daily if you happen to forget to work on the project or you just end up on a unproductive daily meeting.
+</template>
+
+<script lang="js">
+    import Vue from "vue";
+
+    export default Vue.extend({});
+</script>
+
